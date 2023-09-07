a3178c35ffc9a2b1dff856423e7f8ccf9d192c33
Aggrey Osango <hurrycane661@gmail.com>
2023-09-08 01:53:10 +0300

    done

--- main.py
+++ main.py
@@ -2,13 +2,13 @@
 import customtkinter as ctk
 from tkinter import  *
 
-c_radius = 10
-mid = 0.5
-widthx, heighty = 170, 30
+c_radius, mid = 10, 0.5
 
+widthx, heighty = 170, 30
+btns = ["First Name", "Last Name", "Age"]
 # app configuration
 ctk.set_appearance_mode ("light")
-ctk.set_default_color_theme ("#8AAB93")
+ctk.set_default_color_theme ("blue")
 
 
 # root window
@@ -19,17 +19,37 @@ root.title("Helium Notes")
 # Frame for the window
 frame = ctk.CTkFrame (master = root, width = 590, height = 510)
 
+# variable text
+var_text = StringVar ()
 # first name entry obj
-fname = ctk.CTk.Entry (master = frame, width = widthx, height = heighty, placeholder_text = "First Name", corner_radius = c_radius )
-lname = ctk.CTk.Entry (master = frame, width = widthx, height = heighty, placeholder_text = "Last Name", corner_radius = c_radius)
-age   = ctk.CTk.Entry (master = frame, widthx= widthx, height = heighty, placeholder_text = "0", corner_radius = c_radius)
+fname = ctk.CTkEntry (master = frame, width = widthx, height = heighty, placeholder_text = "First Name", corner_radius = c_radius )
+lname = ctk.CTkEntry (master = frame, width = widthx, height = heighty, placeholder_text = "Last Name", corner_radius = c_radius)
+age   = ctk.CTkEntry (master = frame, width = widthx, height = heighty, placeholder_text = "0", corner_radius = c_radius)
 
+def log(value):
+    ffname = str(fname.get ())
+    llname = str (lname.get ())
+    a = str (age.get ())
+    if value == "First Name":
+        var_text.set (f"First Name: {ffname}")
+
+    if value == "Last Name":
+        var_text.set (f"Last Name: {llname}")
+
+    if value == "Age":
+        var_text.set (f"Age: {a}")
+
+
+msg_label = ctk.CTkLabel (master = frame, width = widthx, height = heighty, corner_radius = c_radius, textvariable = var_text, bg_color = "yellow")
+
+seg_btn = ctk.CTkSegmentedButton (master = frame, width = widthx, height = heighty, corner_radius = c_radius, values = btns, command = log)
 
 frame.pack  (padx=10, pady = 10)
-fname.place (relx = mid, rely = mid - 0.2, anchor = CENTER)
-lname.place (relx = mid, rely = mid + 0.1, anchor = CENTER)
-age.place   (relx = mid, rely = mid + 0.2, anchor = CENTER)
-
+fname.place (relx = mid, rely = 0.2 , anchor = CENTER)
+lname.place (relx = mid, rely = 0.3, anchor = CENTER)
+age.place   (relx = mid, rely = 0.4, anchor = CENTER)
+seg_btn.place (relx = mid, rely = 0.5, anchor = CENTER)
+msg_label.place(relx = mid, rely =0.6, anchor = CENTER)
 
 if __name__ == '__main__':
     root.mainloop()
